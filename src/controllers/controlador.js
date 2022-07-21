bc133930429d43a600fd6e20cbf4b329f687475d
ToxicSymphony <brayan_stiv96@hotmail.com>
2022-07-21 17:40:52 -0500

    se modifica el index y se agrega la tienda y los controladores

---
+++ src/controllers/controlador.js
@@ -0,0 +1,13 @@
+//creando variables
+
+let nombreUsuario="Brayan Ospina"
+const EDADU_USUARIO=33
+let estaturaUsuario=1.84
+let hinchaDelVerde=false
+
+console.log(nombreUsuario)
+
+edadUsuario=20
+
+console.log(EDADU_USUARIO)
+
