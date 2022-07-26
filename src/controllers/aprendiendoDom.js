f02940330af1a5852909fdc19d0ce9e9bedf05ec
ToxicSymphony <brayan_stiv96@hotmail.com>
2022-07-26 17:34:00 -0500

    se crean ejemplos de DOM y se agregan algunas imagenes al objeto producto

---
+++ src/controllers/aprendiendoDom.js
@@ -0,0 +1,16 @@
+//mcontrolar etiquetas es controloar la interface grafica
+//1. manipular el contenido de una etiqueta, la etiquetas poseen dos tipos datos: texto o fuente.
+
+//pasos para controlar una etiqueta desde js
+//1. controlando el contenido
+
+let etiquetaTitulo = document.getElementById ("titulo")
+let etiquetaImagen = document.getElementById ("imagen")
+
+//1.1 manipulando el texto del elemento 
+
+etiquetaTitulo.textContent="jefe usted es como mi padre"
+
+//1.2 
+
+etiquetaImagen.src="https://firebasestorage.googleapis.com/v0/b/tiendasurabs.appspot.com/o/nave3.2.jpg?alt=media&token=edf5be91-c66e-4f86-a344-2aaf0af93dee"
--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,13 +1,59 @@
 //creando variables
 
-let nombreUsuario="Brayan Ospina"
-const EDADU_USUARIO=33
-let estaturaUsuario=1.84
-let hinchaDelVerde=false
+// let nombreUsuario="Brayan Ospina"
+// const EDAD_USUARIO=33
+// let estaturaUsuario=1.84
+// let hinchaDelVerde=false
 
-console.log(nombreUsuario)
+// console.log(nombreUsuario)
 
-edadUsuario=20
+// edadUsuario=20
 
-console.log(EDADU_USUARIO)
+// console.log(EDAD_USUARIO)
+
+//mercando con el profe
+
+// arreglos de datos, todos los nombres de los arreglos se deben escribir en plural, cuando l corchete esta vacio se le dice "corchete vacio". Cada dato dentro del arreglo se llama elemnto y cada elemento tiene una posicion, la posicion en muchos lenguajes se conocen como "index"
+
+//arreglo tradicional
+
+// let productos = ["arroz","leche","huevo","carne","pollo","galletas","panela","cafe","tapa roja","cerveza","tomates","papas","lechuga","frijoles","aceite"]
+
+let numeros = [1,2,8,9,45]
+
+let nombres = ["juan","catalina","mario","carlos"]
+
+let contagios = [true,false,false,false,true]
+
+//1.1 accediendo a todos los datos o elementos de un arreglo
+
+console.log(numeros)
+
+//1.2 accediendo a un solo elemento del arreglo
+
+console.log (numeros[3])
+
+//2. arreglos orientados a objetos arreglos poo
+
+let notas = Array(3.5,2.8,3.9,5)
+console.log (notas)
+console.log (notas[1])
+
+//variables especiales para guardar varios datos de tipos diferentes
+//3.objetos
+let ObjPersona = {
+    nombre:"juan", 
+    apellido:"ruiz", 
+    edad:25, 
+    cedula: 34534234 
+}
+
+//3.1 accediendo a los datos de un objeto
+console.log(ObjPersona)
+
+//3.2 accediendo a un solo dato de un objeto
+
+console.log (ObjPersona.nombre + " " + " " +ObjPersona.apellido + " " + " " +ObjPersona.edad + " " + " " + ObjPersona.cedula)
+
+
 
